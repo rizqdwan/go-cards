7038d8521690dcfdf7b1c55d3ac998e64dcbf5ec
Rizqdwan <rizqdwandhuhakbar@gmail.com>
2025-11-15 22:15:09 +0700

    feat: struct data

---
+++ structs/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+
+// alex := person{firstName: "Alex", lastName: "Person"}
+
+// fmt.Println(alex)
+
+var alex person
+fmt.Println(alex)
+
+
+
+
+}
